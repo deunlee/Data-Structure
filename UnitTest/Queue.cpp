4c12ac14af53f7be7be792c72b575e138c088898
Deun Lee <54697735+deunlee@users.noreply.github.com>
2020-05-24 18:01:28 +0900

    [Test] Add Unit Test for Queue

---
+++ UnitTest/Queue.cpp
@@ -0,0 +1,116 @@
+#include "CppUnitTest.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+#include "../Queue/Queue.h"
+#include "../Queue/Queue.cpp"
+
+TEST_CLASS(Queue) {
+public:
+
+    TEST_METHOD(Basic) {
+        Deun::Queue q(10);
+
+        Assert::AreEqual(q.getSize(), 10U);
+        Assert::AreEqual(q.getCount(), 0U);
+        Assert::IsTrue(q.isEmpty());
+        Assert::IsFalse(q.isFull());
+
+        Assert::IsTrue(q.enqueue(1));
+        Assert::IsTrue(q.enqueue(2));
+        Assert::IsTrue(q.enqueue(3));
+
+        Assert::AreEqual(q.getSize(), 10U);
+        Assert::AreEqual(q.getCount(), 3U);
+        Assert::IsFalse(q.isEmpty());
+        Assert::IsFalse(q.isFull());
+
+        Assert::AreEqual(q.dequeue(), 1);
+        Assert::AreEqual(q.dequeue(), 2);
+        Assert::AreEqual(q.dequeue(), 3);
+
+        Assert::AreEqual(q.getSize(), 10U);
+        Assert::AreEqual(q.getCount(), 0U);
+        Assert::IsTrue(q.isEmpty());
+        Assert::IsFalse(q.isFull());
+    }
+
+    TEST_METHOD(TooManyEnqueue) {
+        Deun::Queue q(2);
+
+        Assert::IsTrue(q.enqueue(1));
+        Assert::IsTrue(q.enqueue(2));
+
+        Assert::IsFalse(q.enqueue(3));
+    }
+
+    TEST_METHOD(TooManyDequeue) {
+        Deun::Queue q(2);
+
+        Assert::IsTrue(q.enqueue(1));
+        Assert::IsTrue(q.enqueue(2));
+        Assert::AreEqual(q.dequeue(), 1);
+        Assert::AreEqual(q.dequeue(), 2);
+
+        Assert::ExpectException<Deun::QueueError>([&] { q.dequeue(); });
+
+        try {
+            q.dequeue();
+        }
+        catch (Deun::QueueError err) {
+            if (err == Deun::QueueError::QUEUE_IS_EMPTY) {
+                return;
+            }
+        }
+
+        Assert::Fail();
+    }
+
+    TEST_METHOD(Peek) {
+        Deun::Queue q(2);
+
+        Assert::IsTrue(q.enqueue(1));
+        Assert::IsTrue(q.enqueue(2));
+
+        Assert::AreEqual(q.peek(), 1);
+        Assert::AreEqual(q.dequeue(), 1);
+        Assert::AreEqual(q.peek(), 2);
+        Assert::AreEqual(q.dequeue(), 2);
+
+        Assert::ExpectException<Deun::QueueError>([&] { q.peek(); });
+    }
+
+    TEST_METHOD(Cycle) {
+        Deun::Queue q(3);
+
+        for (int i = 0; i < 10; i++) {
+            Assert::IsTrue(q.enqueue(1));
+            Assert::IsTrue(q.enqueue(2));
+            Assert::IsTrue(q.enqueue(3));
+            Assert::AreEqual(q.getCount(), 3U);
+            Assert::IsFalse(q.isEmpty());
+            Assert::IsTrue(q.isFull());
+            Assert::AreEqual(q.dequeue(), 1);
+            Assert::AreEqual(q.dequeue(), 2);
+            Assert::AreEqual(q.dequeue(), 3);
+            Assert::AreEqual(q.getCount(), 0U);
+
+            Assert::IsTrue(q.enqueue(4));
+            Assert::AreEqual(q.getCount(), 1U);
+            Assert::AreEqual(q.peek(), 4);
+            Assert::IsTrue(q.enqueue(5));
+            Assert::AreEqual(q.getCount(), 2U);
+            Assert::AreEqual(q.dequeue(), 4);
+            Assert::IsTrue(q.enqueue(6));
+            Assert::AreEqual(q.getCount(), 2U);
+            Assert::AreEqual(q.dequeue(), 5);
+            Assert::IsTrue(q.enqueue(7));
+            Assert::AreEqual(q.dequeue(), 6);
+            Assert::AreEqual(q.getCount(), 1U);
+            Assert::AreEqual(q.dequeue(), 7);
+            Assert::IsTrue(q.enqueue(8));
+            Assert::AreEqual(q.dequeue(), 8);
+            Assert::IsTrue(q.isEmpty());
+            Assert::IsFalse(q.isFull());
+        }
+    }
+};
---
+++ UnitTest/Stack.cpp
@@ -0,0 +1,116 @@
+#include "CppUnitTest.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+#include "../Stack/Stack.h"
+#include "../Stack/Stack.cpp"
+
+TEST_CLASS(Stack) {
+public:
+
+    TEST_METHOD(Basic) {
+        Deun::Stack s(10);
+
+        Assert::AreEqual(s.getSize(), 10U);
+        Assert::AreEqual(s.getCount(), 0U);
+        Assert::IsTrue(s.isEmpty());
+        Assert::IsFalse(s.isFull());
+
+        Assert::IsTrue(s.push(1));
+        Assert::IsTrue(s.push(2));
+        Assert::IsTrue(s.push(3));
+
+        Assert::AreEqual(s.getSize(), 10U);
+        Assert::AreEqual(s.getCount(), 3U);
+        Assert::IsFalse(s.isEmpty());
+        Assert::IsFalse(s.isFull());
+
+        Assert::AreEqual(s.pop(), 3);
+        Assert::AreEqual(s.pop(), 2);
+        Assert::AreEqual(s.pop(), 1);
+
+        Assert::AreEqual(s.getSize(), 10U);
+        Assert::AreEqual(s.getCount(), 0U);
+        Assert::IsTrue(s.isEmpty());
+        Assert::IsFalse(s.isFull());
+    }
+
+    TEST_METHOD(TooManyPush) {
+        Deun::Stack s(2);
+
+        Assert::IsTrue(s.push(1));
+        Assert::IsTrue(s.push(2));
+
+        Assert::IsFalse(s.push(3));
+    }
+
+    TEST_METHOD(TooManyPop) {
+        Deun::Stack s(2);
+
+        Assert::IsTrue(s.push(1));
+        Assert::IsTrue(s.push(2));
+        Assert::AreEqual(s.pop(), 2);
+        Assert::AreEqual(s.pop(), 1);
+
+        Assert::ExpectException<Deun::StackError>([&] { s.pop(); });
+
+        try {
+            s.pop();
+        }
+        catch (Deun::StackError err) {
+            if (err == Deun::StackError::STACK_IS_EMPTY) {
+                return;
+            }
+        }
+
+        Assert::Fail();
+    }
+
+    TEST_METHOD(Peek) {
+        Deun::Stack s(2);
+
+        Assert::IsTrue(s.push(1));
+        Assert::IsTrue(s.push(2));
+
+        Assert::AreEqual(s.peek(), 2);
+        Assert::AreEqual(s.pop(), 2);
+        Assert::AreEqual(s.peek(), 1);
+        Assert::AreEqual(s.pop(), 1);
+
+        Assert::ExpectException<Deun::StackError>([&] { s.peek(); });
+    }
+
+    TEST_METHOD(Cycle) {
+        Deun::Stack s(3);
+
+        for (int i = 0; i < 10; i++) {
+            Assert::IsTrue(s.push(1));
+            Assert::IsTrue(s.push(2));
+            Assert::IsTrue(s.push(3));
+            Assert::AreEqual(s.getCount(), 3U);
+            Assert::IsFalse(s.isEmpty());
+            Assert::IsTrue(s.isFull());
+            Assert::AreEqual(s.pop(), 3);
+            Assert::AreEqual(s.pop(), 2);
+            Assert::AreEqual(s.pop(), 1);
+            Assert::AreEqual(s.getCount(), 0U);
+
+            Assert::IsTrue(s.push(4));
+            Assert::AreEqual(s.getCount(), 1U);
+            Assert::AreEqual(s.peek(), 4);
+            Assert::IsTrue(s.push(5));
+            Assert::AreEqual(s.getCount(), 2U);
+            Assert::AreEqual(s.pop(), 5);
+            Assert::IsTrue(s.push(6));
+            Assert::AreEqual(s.getCount(), 2U);
+            Assert::AreEqual(s.pop(), 6);
+            Assert::IsTrue(s.push(7));
+            Assert::AreEqual(s.pop(), 7);
+            Assert::AreEqual(s.getCount(), 1U);
+            Assert::AreEqual(s.pop(), 4);
+            Assert::IsTrue(s.push(8));
+            Assert::AreEqual(s.pop(), 8);
+            Assert::IsTrue(s.isEmpty());
+            Assert::IsFalse(s.isFull());
+        }
+    }
+};
