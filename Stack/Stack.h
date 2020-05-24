bbec51c4e80e2f82c7b26a7e5282bd8f06ba4917
Deun Lee <54697735+deunlee@users.noreply.github.com>
2020-05-24 16:14:22 +0900

    [Stack] Replace tab to 4 spaces

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -4,32 +4,32 @@
 #include <new>
 
 namespace Deun {
-	enum class StackError {
-		STACK_ALLOCATE_FAILED,
-		STACK_IS_FULL,
-		STACK_IS_EMPTY,
-	};
+    enum class StackError {
+        STACK_ALLOCATE_FAILED,
+        STACK_IS_FULL,
+        STACK_IS_EMPTY,
+    };
 
-	class Stack {
-	private:
-		int* elements;
-		unsigned long size;
-		unsigned long top;
+    class Stack {
+    private:
+        int* elements;
+        unsigned long size;
+        unsigned long top;
 
-	public:
-		Stack(unsigned long size = 100);
-		~Stack();
+    public:
+        Stack(unsigned long size = 100);
+        ~Stack();
 
-		bool isFull();
-		bool isEmpty();
+        bool isFull();
+        bool isEmpty();
 
-		unsigned long getSize();
-		unsigned long getCount();
+        unsigned long getSize();
+        unsigned long getCount();
 
-		bool push(int element);
-		int pop();
-		int peek();
-	};
+        bool push(int element);
+        int pop();
+        int peek();
+    };
 }
 
 #endif
--- StackTest/StackTest.cpp
+++ StackTest/StackTest.cpp
@@ -9,79 +9,79 @@ TEST_CLASS(UnitTest)
 {
 public:
 
-	TEST_METHOD(Basic)
-	{
-		Stack s(10);
+    TEST_METHOD(Basic)
+    {
+        Stack s(10);
 
-		Assert::AreEqual(s.getSize(), 10UL);
-		Assert::AreEqual(s.getCount(), 0UL);
-		Assert::IsTrue(s.isEmpty());
-		Assert::IsFalse(s.isFull());
+        Assert::AreEqual(s.getSize(), 10UL);
+        Assert::AreEqual(s.getCount(), 0UL);
+        Assert::IsTrue(s.isEmpty());
+        Assert::IsFalse(s.isFull());
 		
-		Assert::IsTrue(s.push(1));
-		Assert::IsTrue(s.push(2));
-		Assert::IsTrue(s.push(3));
+        Assert::IsTrue(s.push(1));
+        Assert::IsTrue(s.push(2));
+        Assert::IsTrue(s.push(3));
 
-		Assert::AreEqual(s.getSize(), 10UL);
-		Assert::AreEqual(s.getCount(), 3UL);
-		Assert::IsFalse(s.isEmpty());
-		Assert::IsFalse(s.isFull());
+        Assert::AreEqual(s.getSize(), 10UL);
+        Assert::AreEqual(s.getCount(), 3UL);
+        Assert::IsFalse(s.isEmpty());
+        Assert::IsFalse(s.isFull());
 
-		Assert::AreEqual(s.pop(), 3);
-		Assert::AreEqual(s.pop(), 2);
-		Assert::AreEqual(s.pop(), 1);
+        Assert::AreEqual(s.pop(), 3);
+        Assert::AreEqual(s.pop(), 2);
+        Assert::AreEqual(s.pop(), 1);
 
-		Assert::AreEqual(s.getSize(), 10UL);
-		Assert::AreEqual(s.getCount(), 0UL);
-		Assert::IsTrue(s.isEmpty());
-		Assert::IsFalse(s.isFull());
-	}
+        Assert::AreEqual(s.getSize(), 10UL);
+        Assert::AreEqual(s.getCount(), 0UL);
+        Assert::IsTrue(s.isEmpty());
+        Assert::IsFalse(s.isFull());
+    }
 
-	TEST_METHOD(TooManyPush)
-	{
-		Stack s(2);
+    TEST_METHOD(TooManyPush)
+    {
+        Stack s(2);
 
-		Assert::IsTrue(s.push(1));
-		Assert::IsTrue(s.push(2));
+        Assert::IsTrue(s.push(1));
+        Assert::IsTrue(s.push(2));
 
-		Assert::IsFalse(s.push(3));
-	}
+        Assert::IsFalse(s.push(3));
+    }
 
-	TEST_METHOD(TooManyPop)
-	{
-		Stack s(2);
+    TEST_METHOD(TooManyPop)
+    {
+        Stack s(2);
 
-		Assert::IsTrue(s.push(1));
-		Assert::IsTrue(s.push(2));
-		Assert::AreEqual(s.pop(), 2);
-		Assert::AreEqual(s.pop(), 1);
+        Assert::IsTrue(s.push(1));
+        Assert::IsTrue(s.push(2));
+        Assert::AreEqual(s.pop(), 2);
+        Assert::AreEqual(s.pop(), 1);
 
-		Assert::ExpectException<StackError>([&] { s.pop(); });
+        Assert::ExpectException<StackError>([&] { s.pop(); });
 
-		try {
-			s.pop();
-		}
-		catch (StackError err) {
-			if (err == StackError::STACK_IS_EMPTY) {
-				return;
-			}
-		}
+        try {
+            s.pop();
+        }
+        catch (StackError err) {
+            if (err == StackError::STACK_IS_EMPTY) {
+                return;
+            }
+        }
 
-		Assert::Fail();
-	}
+        Assert::Fail();
+    }
 
-	TEST_METHOD(Peek)
-	{
-		Stack s(2);
+    TEST_METHOD(Peek)
+    {
+        Stack s(2);
 
-		Assert::IsTrue(s.push(1));
-		Assert::IsTrue(s.push(2));
+        Assert::IsTrue(s.push(1));
+        Assert::IsTrue(s.push(2));
 
-		Assert::AreEqual(s.peek(), 2);
-		Assert::AreEqual(s.pop(), 2);
-		Assert::AreEqual(s.peek(), 1);
-		Assert::AreEqual(s.pop(), 1);
+        Assert::AreEqual(s.peek(), 2);
+        Assert::AreEqual(s.pop(), 2);
+        Assert::AreEqual(s.peek(), 1);
+        Assert::AreEqual(s.pop(), 1);
 
-		Assert::ExpectException<StackError>([&] { s.peek(); });
-	}
+        Assert::ExpectException<StackError>([&] { s.peek(); });
+    }
 };
