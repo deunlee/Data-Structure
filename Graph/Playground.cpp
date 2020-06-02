cf7de13aa9ca9417c67747487f51bfc67c80199b
Deun Lee <54697735+deunlee@users.noreply.github.com>
2020-06-02 13:31:28 +0900

    [Graph] Fix playground

--- Graph/Playground.cpp
+++ Graph/Playground.cpp
@@ -4,109 +4,78 @@ using namespace std;
 #include "Graph.h"
 using namespace Deun;
 
-void adjacencyMatrixPG() {
-    AdjacencyMatrix adjMatrix(5);
-    int menu, value1, value2;
-
-    cout << boolalpha;
-    cout << "< AdjacencyMatrix Playground >" << endl << endl;
-
-    while (1) {
-        adjMatrix.print();
-        cout << ">>> insertVertex=1, insertEdge=2, hasVertex=3, hasEdge=4 : ";
-        cin >> menu;
-
-        switch (menu) {
-        case 1:
-            try {
-                cout << ">>> insertVertex(): " << adjMatrix.insertVertex();
-            }
-            catch (GraphError err) {
-                cout << ">>> insertVertex(): Error Code " << (int)err;
-            }
-            break;
-
-        case 2:
-            cout << ">>> from and to : ";
-            cin >> value1 >> value2;
-            cout << ">>> insertEdge(" << value1 << ", " << value2 << "): ";
-            cout << adjMatrix.insertEdge(value1, value2);
-            break;
-
-        case 3:
-            cout << ">>> index : ";
-            cin >> value1;
-            cout << ">>> hasVertex(" << value1 << "): " << adjMatrix.hasVertex(value1);
-            break;
-
-        case 4:
-            cout << ">>> from and to : ";
-            cin >> value1 >> value2;
-            cout << ">>> hasEdge(" << value1 << ", " << value2 << "): ";
-            cout << adjMatrix.hasEdge(value1, value2);
-            break;
-
-        default:
-            cout << ">>> unknown command";
-        }
-
-        cout << endl << "--------------------------------------------------" << endl;
-    }
-}
-
-void adjacencyListPG() {
-    AdjacencyList adjList(5);
-    int menu, value1, value2;
-
-    cout << boolalpha;
-    cout << "< AdjacencyList Playground >" << endl << endl;
-
-    while (1) {
-        adjList.print();
-        cout << ">>> insertVertex=1, insertEdge=2, hasVertex=3, hasEdge=4 : ";
-        cin >> menu;
-
-        switch (menu) {
-        case 1:
-            try {
-                cout << ">>> insertVertex(): " << adjList.insertVertex();
-            }
-            catch (GraphError err) {
-                cout << ">>> insertVertex(): Error Code " << (int)err;
-            }
-            break;
-
-        case 2:
-            cout << ">>> from and to : ";
-            cin >> value1 >> value2;
-            cout << ">>> insertEdge(" << value1 << ", " << value2 << "): ";
-            cout << adjList.insertEdge(value1, value2);
-            break;
-
-        case 3:
-            cout << ">>> index : ";
-            cin >> value1;
-            cout << ">>> hasVertex(" << value1 << "): " << adjList.hasVertex(value1);
-            break;
-
-        case 4:
-            cout << ">>> from and to : ";
-            cin >> value1 >> value2;
-            cout << ">>> hasEdge(" << value1 << ", " << value2 << "): ";
-            cout << adjList.hasEdge(value1, value2);
-            break;
-
-        default:
-            cout << ">>> unknown command";
-        }
-
-        cout << endl << "--------------------------------------------------" << endl;
-    }
-}
-
 int main() {
-    //adjacencyMatrixPG();
-    adjacencyListPG();
+    Graph* graph = nullptr;
+    int menu, value1, value2;
+
+    cout << boolalpha;
+    cout << "< Graph Playground >" << endl << endl;
+
+    cout << ">>> Select graph type (AdjacencyMatrix=1, AdjacencyList=2) : ";
+    cin >> menu;
+
+    switch (menu) {
+    case 1:
+        graph = new AdjacencyMatrix(5);
+        break;
+
+    case 2:
+        graph = new AdjacencyList(5);
+        break;
+
+    default:
+        cout << ">>> Unknown type.";
+        return 0;
+    }
+
+    cout << endl;
+
+    while (1) {
+        graph->print();
+        cout << ">>> insertVertex=1, insertEdge=2, hasVertex=3, hasEdge=4, exit=5 : ";
+        cin >> menu;
+
+        switch (menu) {
+        case 1:
+            try {
+                cout << ">>> insertVertex(): " << graph->insertVertex();
+            }
+            catch (GraphError err) {
+                cout << ">>> insertVertex(): Error Code " << (int)err;
+            }
+            break;
+
+        case 2:
+            cout << ">>> from and to : ";
+            cin >> value1 >> value2;
+            cout << ">>> insertEdge(" << value1 << ", " << value2 << "): ";
+            cout << graph->insertEdge(value1, value2);
+            break;
+
+        case 3:
+            cout << ">>> index : ";
+            cin >> value1;
+            cout << ">>> hasVertex(" << value1 << "): " << graph->hasVertex(value1);
+            break;
+
+        case 4:
+            cout << ">>> from and to : ";
+            cin >> value1 >> value2;
+            cout << ">>> hasEdge(" << value1 << ", " << value2 << "): ";
+            cout << graph->hasEdge(value1, value2);
+            break;
+
+        case 5:
+            cout << ">>> Exit..." << endl;
+            delete graph;
+            return 0;
+
+        default:
+            cout << ">>> Unknown command.";
+        }
+
+        cout << endl << "==================================================" << endl;
+    }
 
     return 0;
 }
